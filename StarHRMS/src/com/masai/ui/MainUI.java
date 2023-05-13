6af6a19020de98abd5bcf87fb9a35a8cce584b1d
Sakshi0704 <banichoudhary0704@gmail.com>
2023-05-13 23:44:54 +0530

    modifed comment on methods

--- StarHRMS/src/com/masai/ui/MainUI.java
+++ StarHRMS/src/com/masai/ui/MainUI.java
@@ -5,31 +5,49 @@ import com.masai.color.ConsoleColor;
 
 public class MainUI {
 
+	/**
+	 * Displays the login screen for the admin panel and handles the user's input.
+	 *
+	 * @param sr The Scanner object used to read user input.
+	 * 
+	 * @throws InterruptedException If there is an interruption during thread execution.
+	 */
 	static void adminLink(Scanner sr) throws InterruptedException {
 		String choice = "0";
 		do {
+			// Print the login screen header
 			System.out.println("\n");
 			System.out.println(ConsoleColor.BLACK_BACKGROUND_BRIGHT + ConsoleColor.BLACK_BOLD
 					+ "+--------------------------------------------------+");
 			System.out.println("|              LogIn To Admin Panel               |");
 			System.out.println("+--------------------------------------------------+" + ConsoleColor.RESET);
 
+			// Display the available options
 			System.out.println("+--------------------------------------------------+");
 			System.out.println("| Press 1: LogIn To Admin Panel                    |");
 			System.out.println("| Press 0: Go back to Home Page                    |");
 			System.out.println("+--------------------------------------------------+");
 			System.out.print("Enter your selection: ");
+			
+			
+			// Read the user's input
 			choice = sr.next();
+			
+			
+			 // Process the user's choice
 			switch (choice) {
 			case "1":
+				// Call the admin login method from the AdminUI class
 				AdminUI.adminLogInUI(sr);
 				break;
 			case "0":
+				// Display a farewell message and exit the loop
 				System.out.println(ConsoleColor.GREEN + "----------------------------------------------------");
 				System.out.println("Thanks For Visiting!");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
 				break;
 			default:
+				// Display an error message for an invalid selection
 				System.out.println(ConsoleColor.RED + "----------------------------------------------------");
 				System.out.println("Oops! Try again with a valid selection");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
@@ -38,31 +56,47 @@ public class MainUI {
 		System.out.println();
 	}
 
+	
+	/**
+	 * Displays the login screen for the employee page and handles the user's input.
+	 *
+	 * @param sr The Scanner object used to read user input.
+	 */
 	static void employeeLink(Scanner sr) {
 		String choice = "0";
 		do {
+			// Display the login screen header
 			System.out.println("\n");
 			System.out.println(ConsoleColor.BLACK_BACKGROUND_BRIGHT + ConsoleColor.BLACK_BOLD
 					+ "+--------------------------------------------------+");
 			System.out.println("|              LogIn To Employee Page             |");
 			System.out.println("+--------------------------------------------------+" + ConsoleColor.RESET);
 
+			// Display the available options
 			System.out.println("+--------------------------------------------------+");
 			System.out.println("| Press 1: LogIn To Employee Page                  |");
 			System.out.println("| Press 0: Go back to Home Page                    |");
 			System.out.println("+--------------------------------------------------+");
 			System.out.print("Enter your selection: ");
+			
+			
+			// Read the user's input
 			choice = sr.next();
+			
+			// Process the user's choice
 			switch (choice) {
 			case "1":
+				// Call the employee login method from the EmployeeUI class
 				EmployeeUI.empLogInUI(sr);
 				break;
 			case "0":
+				// Display a farewell message and exit the loop
 				System.out.println(ConsoleColor.GREEN + "----------------------------------------------------");
 				System.out.println("Thanks For Visiting!");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
 				break;
 			default:
+				// Display an error message for an invalid selection
 				System.out.println(ConsoleColor.RED + "----------------------------------------------------");
 				System.out.println("Oops! Try again with a valid selection");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
@@ -72,8 +106,24 @@ public class MainUI {
 
 	}
 
+	
+	
+	/**
+	 * The main method of the StarHR Management System program.
+	 * 
+	 * It displays the welcome message and provides a menu for users to choose 
+	 * between the admin page, employee page, or exiting the program.
+	 *
+	 * @param args The command-line arguments passed to the program.
+	 * 
+	 * @throws InterruptedException If there is an interruption during thread execution.
+	 */
 	public static void main(String args[]) throws InterruptedException {
+		
+		// Create a Scanner object to read user input
 		Scanner sr = new Scanner(System.in);
+		
+		// Display the welcome message and system title
 		System.out.println("");
 		System.out.println(ConsoleColor.FOREST_GREEN_BACKGROUND + ConsoleColor.BLACK_BOLD
 				+ "+--------------------------------------------------+");
@@ -83,6 +133,7 @@ public class MainUI {
 
 		String choice = "0";
 		do {
+			// Display the menu options
 			System.out.println("\n");
 			System.out.println(ConsoleColor.BLACK_BACKGROUND_BRIGHT + ConsoleColor.BLACK_BOLD
 					+ "+--------------------------------------------------+");
@@ -93,20 +144,28 @@ public class MainUI {
 			System.out.println("| Press 0: Exit                                    |");
 			System.out.println("+--------------------------------------------------+");
 			System.out.print("Enter your selection: ");
+			
+			// Read the user's input
 			choice = sr.next();
+			
+			// Process the user's choice
 			switch (choice) {
 			case "1":
+				// Call the adminLink method to navigate to the admin page
 				adminLink(sr);
 				break;
 			case "2":
+				// Call the employeeLink method to navigate to the employee page
 				employeeLink(sr);
 				break;
 			case "0":
+				// Display a farewell message and exit the program
 				System.out.println(ConsoleColor.GREEN + "----------------------------------------------------");
 				System.out.println("Thanks For Visiting!");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
 				break;
 			default:
+				// Display an error message for an invalid selection and wait for 1 second before displaying the menu again
 				System.out.println(ConsoleColor.RED + "----------------------------------------------------");
 				System.out.println("Oops! Try again with a valid selection");
 				System.out.println("----------------------------------------------------" + ConsoleColor.RESET);
@@ -118,6 +177,7 @@ public class MainUI {
 
 		System.out.println("");
 
+		// Close the Scanner object
 		sr.close();
 
 	}
